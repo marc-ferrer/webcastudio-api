18e65f1f858fe172a1f129cad362bcdfd816565e
MarcVancast <mark.mff@gmail.com>
2014-02-21 13:15:36 +0100

    fixes #7

--- test/test.js
+++ test/test.js
@@ -1,53 +1,9 @@
 var assert = require('assert'),
 		should = require('should'),
-		fs = require('fs'),
-		path = require('path');
-		mongoose = require('mongoose'),
-		config = require('../config/config'),
-		http = require('http'),
-		mocks = require('./mocks.js'),
-		mysql = require('mysql'),
+		mocks = require('./mocks.js')
 		EventResource = require('../app/models/eventresource'),
 		LanguageResource = require('../app/models/languageresource'),
 		SignatureV1 = require('../app/signatures/signaturev1.js');
-
-describe('Connection', function(){
-	before(function(done) {
-		mongoose.connect(config.db,done);
-	});
-	it('should include all models', function (done) {
-		var modelsPath = path.dirname(__dirname) + '/app/models';
-		fs.readdirSync(modelsPath).forEach(function (file) {
-			if (file.indexOf('.js') >= 0) {
-				require(modelsPath + '/' + file);
-			}
-		});
-
-		done();
-	});
-	it('should log in as user', function (done) {
-		//var user = require('../app/models/user');
-		var user = mongoose.model('User');
-		user.isValidUserPassword('marc@vancast.net', 'vancast', done)
-	});
-	it('should connect to mysql DB', function (done) {
-		var connection = mysql.createConnection({
-			host		: 'localhost',
-			user		: 'marc',
-			password: 'vancast'
-		});
-		connection.connect();
-		var sql = 'SELECT event_id FROM ws_api_test.event WHERE acc_id = 1';
-		connection.query(sql, function(err,results){
-			for (var i = 0; i < results.length; i++) {
-				var sql2 = 'SELECT * from ws_api_test.event_part WHERE event_id = ?';
-				connection.query(sql2, results[0].event_id, function(err, results){
-				});
-			};
-			done();
-		});
-	});
-})
 
 describe('generate signature test', function(){
 	var date = 1391778505015,
@@ -60,41 +16,6 @@ describe('generate signature test', function(){
 		assert.equal('c25de9a983aa8b3d6bd4138e3e884a003056c809', generatedSign);
 	})
 })
-
-describe('Signatre', function () {
-	before(function(done){
-		var modelsPath = path.dirname(__dirname) + '/app/models';
-		fs.readdirSync(modelsPath).forEach(function (file) {
-			if (file.indexOf('.js') >= 0) {
-				require(modelsPath + '/' + file);
-			}
-		});
-		done();
-	});
-	it('should do nothing', function (done) {
-		var user = mongoose.model('User'),
-				crypto = require('crypto'),
-				signMiddleware = require('../config/middlewares/apiauth');
-		var date = Date.now();
-		var secret = '9700da79204fd3fd6b5ec3601ea823a20b5c6d38';
-		var hmac = crypto.createHmac('sha1', secret);
-		hmac.setEncoding('hex');
-		hmac.write('UXwIHzOjcLfl2nWE'+date);
-		hmac.end();
-		var signature = hmac.read();
-		//TODO: create a mockRequest object
-		var request = {
-			headers: {
-				'date': date,
-				'accesskey': 'UXwIHzOjcLfl2nWE',
-				'signature': signature,
-				'signatureVersion': 'v1'
-			}
-		};
-		var res = new mocks.MockResponse();
-		signMiddleware.checkRequest(request,res,done);
-	});
-});
 
 describe('Resources tests', function () {
 	var results = [];
